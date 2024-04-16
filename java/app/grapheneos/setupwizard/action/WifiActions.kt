a75ad5dc31ce0dc2f7fb5ae9181cbc4a3b4cd42a
Jitesh Singh <jiteshsingh.jts@gmail.com>
2024-04-16 14:16:45 +0530

    remove unused intent extras from wifi setup invocation
    usage of footerbarmixin in settings app internet setup screen makes some
    of the intent extras useless. so we stop sending them.

--- java/app/grapheneos/setupwizard/action/WifiActions.kt
+++ java/app/grapheneos/setupwizard/action/WifiActions.kt
@@ -11,9 +11,6 @@ object WifiActions {
 
     private const val ACTION_SETUP_INTERNET = "android.settings.SETUP_INTERNET"
     private const val ACTION_SETUP_INTERNET_LEGACY = "android.settings.NETWORK_PROVIDER_SETTINGS"
-    private const val EXTRA_SHOW_BUTTONS = "extra_prefs_show_button_bar"
-    private const val EXTRA_SHOW_SKIP = "extra_prefs_show_skip"
-    private const val EXTRA_BACK_TEXT = "extra_prefs_set_back_text"
     private const val EXTRA_SKIP_TEXT = "extra_prefs_set_skip_text";
     private const val EXTRA_ENABLE_NEXT_ON_CONNECT = "wifi_enable_next_on_connect"
     private const val EXTRA_MODE_WIFI = "setup_wizard_mode_wifi"
@@ -29,10 +26,7 @@ object WifiActions {
         )
         intent.putExtra(EXTRA_SETUP_WIZARD_TITLE, context.getString(R.string.connect_to_wi_fi))
         intent.putExtra(EXTRA_SETUP_WIZARD_DESC, context.getString(R.string.select_a_network))
-        intent.putExtra(EXTRA_SHOW_BUTTONS, true)
-        intent.putExtra(EXTRA_SHOW_SKIP, true)
         intent.putExtra(EXTRA_SKIP_TEXT, context.getString(R.string.set_up_without_wi_fi))
-        intent.putExtra(EXTRA_BACK_TEXT, null as String?) // hides back button
         intent.putExtra(EXTRA_ENABLE_NEXT_ON_CONNECT, true)
         intent.putExtra(EXTRA_MODE_WIFI, true)
         SetupWizard.startActivityForResult(context, intent)
